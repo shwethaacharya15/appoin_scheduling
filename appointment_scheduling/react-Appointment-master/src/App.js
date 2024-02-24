e8935e932262e7905ca059405cba191fb78033fa
shwetha <shwetha.20cs084@sode-edu.in>
2024-02-24 20:41:34 +0530

    shwetha

---
+++ appointment_scheduling/react-Appointment-master/src/App.js
@@ -0,0 +1,89 @@
+import { BiCalendar } from "react-icons/bi";
+import { useEffect, useState, useCallback } from "react";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
+import Search from "./components/Search";
+
+function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
+
+  const filteredAppointment = appointmentList
+    .filter((item) => {
+      return (
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      );
+    })
+    ;
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAppointmentList(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <>
+      <div className="App container mx-auto mt-3 font-thin">
+        <h1 className="text-5xl mb-4">
+          <BiCalendar className="inline-block text-red-400 align-top" />
+          Your Appointments
+        </h1>
+        <AddAppointment
+          onSendAppointment={(appointment) => {
+            setAppointmentList([...appointmentList, appointment]);
+          }}
+          lastId={appointmentList.reduce(
+            (pre, curr) => (Number(curr.id) > pre ? Number(curr.id) : pre),
+            0
+          )}
+        />
+        <Search
+          query={query}
+          onQueryChange={(event) => {
+            setQuery(event.target.value);
+          }}
+          orderBy={orderBy}
+          onOrderByChange={(val) => {
+            setOrderBy(val);
+          }}
+          sortBy={sortBy}
+          onSortBYChange={(val) => {
+            setSortBy(val);
+          }}
+        />
+        <ul className="divide-y divide-gray-200">
+          {filteredAppointment.map((appointment) => {
+            return (
+              <AppointmentInfo
+                onDeleteAppointment={(appointmentId) => {
+                  setAppointmentList(
+                    appointmentList.filter(
+                      (appointment) => appointmentId !== appointment.id
+                    )
+                  );
+                }}
+                appointment={appointment}
+                key={appointment.id}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
